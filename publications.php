12d7f62daa6c340bb2166aac00645286243a6f7a
Robin Wilson <r.t.wilson@rmplc.co.uk>
2010-07-03 10:46:09 -0700

    Added forgotten files from last time

---
+++ publications.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<?php include 'stylesheets.php' ?>
+	<title>Robin Wilson - Publications</title>
+</head>
+<body>
+	<div class="container">
+	<h1 class="header">Robin Wilson</h1>
+	<hr>
+	<h2 class="alt">Publications</h2>
+	<hr>
+	<?php include 'navigation.php' ?>
+	<div class="span-20 last">
+		<p>All of my published academic work is listed below</p>
+		
+		<h6>Conference Papers</h6>
+		<p>Wilson, R.T. and Milton, E.J., 2010, Automated selection of suitable atmospheric calibration sites 
+for satellite imagery, <em>Proceedings of the RSPSoc Annual Conference 2010</em>
+<br/>
+<a href="downloads/WilsonMilton_2010_RSPSoc.pdf">PDF</a> (396kb, 8 pages)</p>
+		<hr/>
+	</div>
+</div>
+<?php include 'footer.php' ?>
+</body>
+</html>
